d4a3d067ba94c63e602a7680e92bc1199c65892c
RonexLemon <ronexondimu@gmail.com>
2024-10-18 09:28:05 +0300

    getfeature

--- server/grpc.go
+++ server/grpc.go
@@ -2,14 +2,29 @@ package server
 
 import (
 	"context"
+	"sync"
 
 	pb "github.com/RonexLemon/RPC/services/genproto/route"
+	"google.golang.org/protobuf/proto"
 )
 
-type routeGuideServer struct{}
+type routeGuideServer struct{
+	pb.UnimplementedRouteGuideServer
+	 savedFeatures  []*pb.Feature
+	 mu  sync.Mutex
+	 routeNotes map[string][]*pb.RouteNote
+
+}
 
 func(s *routeGuideServer) GetFeature(ctx context.Context,point *pb.Point)(*pb.Feature,error){
 	//logic to get feature
+	for _,feature := range s.savedFeatures{
+		if proto.Equal(feature.Location,point){
+			return feature,nil
+		}
+		
+	}
+	return &pb.Feature{Location: point},nil
 }
 
 func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle,stream *pb.RouteGuide_ListFeaturesServer)error{
@@ -20,5 +35,5 @@ func (s *routeGuideServer) RecordRoute(stream *pb.RouteGuide_RecordRouteServer)e
 
 }
 func(s *routeGuideServer)RouteChat(stream *pb.RouteGuide_RouteChatServer)error{
-	
+
 }
